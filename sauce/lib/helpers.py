34e17142f565cfc27c15522212c4240944cb4001
Moritz Schlarb <mail@moritz-schlarb.de>
2012-04-18 09:13:11 +0200

    Replace my own helper functions with webhelper ones

--- sauce/lib/helpers.py
+++ sauce/lib/helpers.py
@@ -4,48 +4,27 @@
 @author: moschlar
 """
 
-from tg import url as tgurl
-from webhelpers import date, feedgenerator, html, number, misc, text
+from datetime import datetime
 
-import re, textwrap
+from tg import url as tgurl
+#from webhelpers import date, feedgenerator, html, number, misc, text
+
+import webhelpers as w
+
+from webhelpers.html.tags import link_to
+from webhelpers.text import truncate
+from webhelpers.date import distance_of_time_in_words
+
+import re
 
 #log = logging.getLogger(__name__)
 
-# shortcut for links
-link_to = html.tags.link_to
+cut = lambda text, max=200: truncate(text, max, whole_word=True)
+strftimedelta = lambda delta, granularity='minute': distance_of_time_in_words(datetime.now(), datetime.now()+delta, granularity)
 
 def link(label, url='', **attrs):
     return link_to(label, tgurl(url), **attrs)
 
-def strftimedelta(delta, format='%D Days %hh:%mm:%ss'):
-    '''Return a string representing the timedelta element.
-    
-    Possible format codes are:
-        %D days
-        %h hours
-        %hh hours with leading zero
-        %m minutes
-        %mm minutes with leading zero
-        %s seconds
-        %ss seconds with leading zero
-    '''
-    totalSeconds = delta.seconds
-    hours, remainder = divmod(totalSeconds, 3600)
-    minutes, seconds = divmod(remainder, 60)
-    
-    result = format.replace('%D', str(delta.days)).\
-            replace('%hh', '%02d' % hours).replace('%mm', '%02d' % minutes).\
-            replace('%ss', '%02d' % seconds).\
-            replace('%h', str(hours)).replace('%m', str(minutes)).\
-            replace('%s', str(seconds))
-    return result
-
 def striphtml(text):
     return re.sub('<[^<]+?>', ' ', text).strip()
-
-def cut(text, max=200):
-    if len(text) < max:
-        return text
-    else:
-        return textwrap.wrap(text, max)[0] + ' ...'
 

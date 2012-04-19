1e182ec0fd7cf550c809f2e6792629caeb8d5553
Moritz Schlarb <mail@moritz-schlarb.de>
2012-04-19 10:47:19 +0200

    Use striptags from genshi for striphtml, since we have to have genshi anyway

--- sauce/lib/helpers.py
+++ sauce/lib/helpers.py
@@ -15,16 +15,17 @@ from webhelpers.html.tags import link_to
 from webhelpers.text import truncate
 from webhelpers.date import distance_of_time_in_words
 
+from genshi.core import striptags
+
 import re
 
 #log = logging.getLogger(__name__)
 
 cut = lambda text, max=200: truncate(text, max, whole_word=True)
 strftimedelta = lambda delta, granularity='minute': distance_of_time_in_words(datetime.now(), datetime.now()+delta, granularity)
+striphtml = striptags
 
 def link(label, url='', **attrs):
     return link_to(label, tgurl(url), **attrs)
 
-def striphtml(text):
-    return re.sub('<[^<]+?>', ' ', text).strip()
 

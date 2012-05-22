fe65abeedf5b43c86e8b352cc98783ea6d62f889
Moritz Schlarb <mail@moritz-schlarb.de>
2012-05-22 18:44:20 +0200

    Remove NewsController, move functionality to RootController

--- sauce/controllers/root.py
+++ sauce/controllers/root.py
@@ -6,16 +6,17 @@
 import os
 
 from tg import expose, flash, require, url, lurl, request, redirect, app_globals as g, abort
+from tg.decorators import paginate
 from tg.i18n import ugettext as _, lazy_ugettext as l_
-from sauce import model
 from repoze.what import predicates
 from tgext.admin.controller import AdminController
 
 from docutils.core import publish_string
 from webhelpers.html.tags import ul, link_to
 
+from sauce import model
 from sauce.lib.base import BaseController
-from sauce.model import DBSession, metadata
+from sauce.model import DBSession, metadata, NewsItem
 from sauce.controllers.error import ErrorController
 from sauce.controllers.assignments import AssignmentsController
 from sauce.controllers.submissions import SubmissionsController
@@ -50,8 +51,6 @@ class RootController(BaseController):
     submissions = SubmissionsController()
     events = EventsController()
     #scores = ScoresController()
-    #tests = TestsController()
-    news = NewsController()
     user = UserController()
     
     @expose('sauce.templates.index')
@@ -82,6 +81,20 @@ class RootController(BaseController):
     @expose('sauce.templates.contact')
     def contact(self):
         return dict(page='contact')
+    
+    @paginate('news')
+    @expose('sauce.templates.news')
+    def news(self, page=1):
+        '''NewsItem listing page'''
+        
+        news_query = NewsItem.query.filter(NewsItem.event_id == None)
+        if not request.teacher:
+            news_query = news_query.filter_by(public=True)
+        
+        #news = Page(news_query, page=page, items_per_page=20)
+        
+        return dict(page='news', news=news_query)
+
     
     @expose('sauce.templates.login')
     def login(self, came_from=lurl('/')):

25a98d987b6a8fc6b3f8ff2c378ed846c488f7a7
Moritz Schlarb <mail@moritz-schlarb.de>
2012-05-31 00:32:35 +0200

    jqplugins.chosen optional

--- sauce/widgets/submission.py
+++ sauce/widgets/submission.py
@@ -8,7 +8,7 @@ Created on 17.03.2012
 import tw2.core as twc
 import tw2.forms as twf
 import tw2.bootstrap as twb
-import tw2.jqplugins.chosen as tw_j_c
+#import tw2.jqplugins.chosen as tw_j_c
 
 from sauce.model import Language, Assignment
 
@@ -19,12 +19,14 @@ class SubmissionForm(twb.HorizontalForm):
     submission_id = twf.HiddenField()
 
     filename = twb.TextField()
-    source = twb.TextArea()
+    source = twb.TextArea(cols=80, rows=8)
     source_file = twb.FileField()
 
-    #language_id = twb.SingleSelectField(options=[], required=True)
-    language_id = tw_j_c.ChosenSingleSelectField(options=[], required=True)
+    language_id = twb.SingleSelectField(options=[], required=True)
+    #language_id = tw_j_c.ChosenSingleSelectField(options=[], required=True)
 
     def prepare(self):
-        self.child.c.language_id.options = self.value.assignment.allowed_languages
+        self.child.c.language_id.options = [(l.id, l.name) for l in self.value.assignment.allowed_languages]
+        if len(self.value.assignment.allowed_languages) > 1:
+            self.child.c.language_id.options.insert(0, ('', ''))
         super(SubmissionForm, self).prepare()

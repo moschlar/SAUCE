87c64a9f2e77d77491fec494a42d331a73fb443a
Moritz Schlarb <mail@moritz-schlarb.de>
2012-06-19 17:54:26 +0200

    More web url testies

--- sauce/tests/functional/test_site.py
+++ sauce/tests/functional/test_site.py
@@ -6,27 +6,42 @@ Created on 14.06.2012
 
 from sauce.tests import TestController
 
-__all__ = ['TestSite']
+# The USERS and PATHS variables are interpreted in a matrix-like style
+# USERS contain valid usernames for the REMOTE_USER variable.
+# The usernames should be sorted hierarchically to make configuration
+# simpler
+# PATHS contains a path and expected statuses for the above usernames.
+# (statuses that are not specified aren't tested, of course)
+# If path[0] is a tuple or list, it will be used for recursive url generation
+
+#   PATH                    ANONYMOUS   STUDENT     TEACHERASS  TEACHER     MANAGER
+USERS = (                   None,       'studenta1','teacherass','teacher','manager')
+PATHS = (
+    # All the static pages
+    (('', ['/', '/index', '/about', '/contact', '/login',
+        ('/docs', ['', '/', '/tests', '/deutsch', '/tips', '/Changelog', '/Roadmap'])
+    ]),
+                            None),
+    # The basic event-sheet-assignment pages
+    (('/events', ['', '/',
+        ('/eip12', ['/',
+            ('/sheets', ['', '/',
+                ('/1', ['', '/',
+                    ('/assignments', ['', '/', '/1'])
+                ])
+            ])
+        ])
+    ]),
+                            None),
+    ('/user',               401,        None),
+    ('/admin',              401,        403,        403,        403,        None),
+    ('/events/eip12/admin', 401,        403,        403,        None)
+    )
 
 
 class TestSite(TestController):
-    paths = ('', ['/', '/index', '/about', '/contact', '/login',
-                ('/docs', ['', '/', '/tests', '/deutsch', '/tips', '/Changelog', '/Roadmap']),
-                ('/events', ['', '/',
-                    ('/eip12', ['/',
-                        ('/sheets', ['', '/',
-                            ('/1', ['', '/',
-                                ('/assignments', ['', '/', '/1'])
-                            ])
-                        ])
-                    ])
-                ])
-            ]
-        )
 
-    def _generate_paths(self, base=None):
-        if base is None:
-            base = self.paths
+    def _generate_paths(self, base):
         a, b = base
         for c in b:
             if isinstance(c, tuple):
@@ -35,10 +50,24 @@ class TestSite(TestController):
             else:
                 yield a + c
 
-    def _test_path(self, path):
-        response = self.app.get(path)
-        assert response.status.startswith('2')
+    def _test_path(self, path, user=None, status=None):
+        if user:
+            env = dict(REMOTE_USER=user)
+        else:
+            env = None
+        self.app.get(path, extra_environ=env, status=status)
 
     def test_paths(self):
-        for path in self._generate_paths():
-            yield self._test_path, path
+        for path in PATHS:
+            p, stati = path[0], path[1:]
+
+            if isinstance(p, tuple):
+                pp = self._generate_paths(p)
+            else:
+                pp = (p, )
+
+            for p in pp:
+                for i, status in enumerate(stati):
+                    user = USERS[i]
+                    yield self._test_path, p, user, status
+

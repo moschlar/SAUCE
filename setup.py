0dbe770c0963da764782258fd762ccb61fcb82a5
Moritz Schlarb <mail@moritz-schlarb.de>
2012-09-11 13:01:03 +0200

    pep8 in setup.py

--- setup.py
+++ setup.py
@@ -18,8 +18,8 @@ except ImportError:
 
 # Fix shutdown errors while installing
 try:
-    import multiprocessing
-    import logging
+    import multiprocessing  # @UnusedImport
+    import logging  # @UnusedImport
 except:
     pass
 
@@ -54,17 +54,17 @@ install_requires = [
     "tw2.jqplugins.chosen",
     "tgext.admin >= 0.5",
     "tgext.crud >= 0.5",
-    "sprox > 0.8b", # Dynamic form widget generation
-#    "tablesorter >= 0.2", # JS-sortable TableBase
-#    "ipython == 0.10.2", # For paster shell, install by hand if necessary
-    "Pygments", # For syntax highlighting
-    "docutils", # For rendering documentation
-    "chardet", # For submission file charset detection
-    "pygmentize > 0.2", # Using ToscaWidgets with a SyntaxHighlighting widget
-    "matplotlib", "libripoff >= 0.2" # For similarity calculation
+    "sprox > 0.8b",  # Dynamic form widget generation
+#    "tablesorter >= 0.2",  # JS-sortable TableBase
+#    "ipython == 0.10.2",  # For paster shell, install by hand if necessary
+    "Pygments",  # For syntax highlighting
+    "docutils",  # For rendering documentation
+    "chardet",  # For submission file charset detection
+    "pygmentize > 0.2",  # Using ToscaWidgets with a SyntaxHighlighting widget
+    "matplotlib", "libripoff >= 0.2"  # For similarity calculation
     ]
 
-if sys.version_info[:2] == (2,4):
+if sys.version_info[:2] == (2, 4):
     print 'WARNING: Your Python version ' + sys.version_info + ' is neither tested nor supported!'
     testpkgs.extend(['hashlib', 'pysqlite'])
     install_requires.extend(['hashlib', 'pysqlite'])

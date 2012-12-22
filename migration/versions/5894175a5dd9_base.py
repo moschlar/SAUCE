db56a51a6b1d90cc1bf0b723f6c6e9a96f6cbe58
Moritz Schlarb <mail@moritz-schlarb.de>
2012-12-22 14:13:41 +0100

    Working alembic setup (use with ./alembic)

---
+++ migration/versions/5894175a5dd9_base.py
@@ -0,0 +1,25 @@
+"""Base Revision
+
+Valid for release version 1.1
+
+Revision ID: 5894175a5dd9
+Revises:
+Create Date: 2012-12-22 13:39:49.091971
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '5894175a5dd9'
+down_revision = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass

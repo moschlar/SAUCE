b863d246c2519bbe1a173942ba549f8a23b31ed4
Moritz Schlarb <moschlar@metalabs.de>
2013-01-17 00:19:35 +0100

    Testsuite fixed - Travis, confirm that

--- sauce/tests/functional/test_site.py
+++ sauce/tests/functional/test_site.py
@@ -14,6 +14,7 @@ from webtest import TestApp
 
 from sauce.tests import teardown_db
 from sauce import model
+import transaction
 
 app = None
 
@@ -30,6 +31,21 @@ def setUpModule():
     cmd = SetupCommand('setup-app')
     cmd.run([test_file])
 
+    # Prepare authz test data
+    subm_100 = model.Submission(id=100, filename=u'subm_100', source=u'subm_100',
+        assignment=model.Assignment.query.filter_by(id=2).one(),
+        user=model.User.query.filter_by(user_name='studentc1').one(),
+        language=model.Language.query.first())
+    subm_101 = model.Submission(id=101, filename=u'subm_101', source=u'subm_101',
+        assignment=model.Assignment.query.filter_by(id=2).one(),
+        user=model.User.query.filter_by(user_name='studentc2').one(),
+        language=model.Language.query.first())
+    subm_102 = model.Submission(id=102, filename=u'subm_102', source=u'subm_102',
+        assignment=model.Assignment.query.filter_by(id=2).one(),
+        user=model.User.query.filter_by(user_name='studente1').one(),
+        language=model.Language.query.first())
+    model.DBSession.add_all((subm_100, subm_101, subm_102))
+    transaction.commit()
 
 def tearDownModule():
     model.DBSession.remove()
@@ -45,7 +61,7 @@ def tearDownModule():
 # If path[0] is a tuple or list, it will be used for recursive url generation
 
 #   PATH                    ANONYMOUS   STUDENT     TUTOR       TEACHER     MANAGER
-USERS = (                   None,       'studenta1','tutor',    'teacher',  'manager')
+USERS = (                   None,       'studentc1','tutor1',    'teacher1',  'manager')
 PATHS = (
     # All the static pages
     (('', ['/', '/index', '/about', '/contact', '/login',
@@ -73,33 +89,32 @@ PATHS = (
     (('/events/demo/admin', ['', '/', '/events', '/sheets', '/assignments', '/tests',
         '/newsitems', '/lessons', '/tutors', '/teams', '/students']),
                             401,        403,        403,        None),
-    (('/events/demo/lessons/1', ['', '/', '/tutor', '/teams', '/students']),
-                            401,        403,        None),
     (('/events/demo/lessons/1', ['', '/',
         ('/submissions', ['', '/',
             ('/sheet/1', ['', '/assignment/1'])
         ])
     ]),
+                            401,        403,        403,        None),
+    (('/events/demo/lessons/2', ['', '/', '/tutor', '/teams', '/students']),
                             401,        403,        None),
     (('/events/demo/lessons/2', ['', '/',
         ('/submissions', ['', '/',
             ('/sheet/1', ['', '/assignment/1'])
         ])
     ]),
-                            401,        403,        403,        None),
-    # WARNING: Submission ids come from websetup/data/course.py
-    # A submission of studenta1, belonging to the lesson of tutor
-    (('/submissions/25', ['', '/', '/show', '/edit', '/result']),
+                            401,        403,        None),
+    # A submission of studentc1, belonging to the lesson of tutor1
+    (('/submissions/100', ['', '/', '/show', '/edit', '/result']),
                             401,        None,       None,       None,       None),
-    ('/submissions/25/judge',
+    ('/submissions/100/judge',
                             401,        403,        None),
-    # Team member of studenta1 submission
-    (('/submissions/26', ['', '/', '/show', '/result']),
+    # Team member of studentc1 submission
+    (('/submissions/101', ['', '/', '/show', '/result']),
                             401,        None),
-    ('/submissions/26/edit',
+    ('/submissions/101/edit',
                             401,        403,        None),
-    # A submission of studentc1, NOT belonging to the lesson of tutor
-    (('/submissions/27', ['', '/', '/show', '/edit', '/result', '/judge']),
+    # A submission of studente1, NOT belonging to the lesson of tutor1
+    (('/submissions/102', ['', '/', '/show', '/edit', '/result', '/judge']),
                             401,        403,        403,        None,       None),
     )
 

b708ac9296a42f5676e1ae7bf8e74325873ade22
Moritz Schlarb <moschlar@metalabs.de>
2013-09-27 14:57:04 +0200

    Refactor run script

--- run.py
+++ run.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+'''Start the Paster server
+
+Given command line arguments will be passed through
+'''
 #
 ## SAUCE - System for AUtomated Code Evaluation
 ## Copyright (C) 2013 Moritz Schlarb
@@ -17,17 +21,9 @@
 ## You should have received a copy of the GNU Affero General Public License
 ## along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-'''Start the Paster server
-
-Given command line arguments will be passed through
-'''
 
 import sys
 from paste.script.serve import ServeCommand
 
 if __name__ == '__main__':
-    if len(sys.argv) == 1:
-        args = ['--reload', 'development.ini']
-    else:
-        args = sys.argv[1:]
-    ServeCommand("serve").run(args)
+    ServeCommand("serve").run(sys.argv[1:] or ['--reload', 'development.ini'])

6d3618d2a31bdbaf47ac99963e37914488b63086
Moritz Schlarb <moschlar@metalabs.de>
2013-12-12 13:14:12 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -105,7 +105,7 @@ extras_require = {
 
 setup(
     name='SAUCE',
-    version='1.5',
+    version='1.5.1',
     description='System for AUtomated Code Evaluation',
     long_description=open(os.path.join(here, 'README.rst')).read(),
     author='Moritz Schlarb',
